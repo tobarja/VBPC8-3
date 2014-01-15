01c0060726242e2630fae4205b64107768c0da10
Andrew Thompson <andrewkt@aktzero.com>
2014-01-14 23:50:34 -0500

    Beginning of functionality.

--- Random Sentences/Form1.vb
+++ Random Sentences/Form1.vb
@@ -1,3 +1,11 @@
 ﻿Public Class Form1
+    Dim builder As SentenceBuilder
 
+    Private Sub btnMakeSentence_Click(sender As Object, e As EventArgs) Handles btnMakeSentence.Click
+        Sentences.Items.Add(builder.Build())
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        builder = New SentenceBuilder()
+    End Sub
 End Class
---
+++ Random Sentences/SentenceBuilder.vb
@@ -0,0 +1,23 @@
+﻿Imports System.Text
+  
+Public Class SentenceBuilder
+    Dim rand As Random
+    Dim Articles As New List(Of String) From {"the", "a", "an"}
+    Dim Nouns As New List(Of String) From {"apple"}
+
+    Public Sub New()
+        rand = New Random()
+    End Sub
+
+    Function Build() As String
+        Dim sb As StringBuilder = New StringBuilder()
+
+        sb.Append(getRandomFromList(Articles))
+        Return sb.ToString()
+    End Function
+
+    Private Function getRandomFromList(wordlist As List(Of String)) As String
+        Return wordlist(rand.Next(wordlist.Count))
+    End Function
+
+End Class
